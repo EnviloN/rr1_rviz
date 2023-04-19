d2c7a20fdedaaa9d079140173b3b29253c22113e
Jan Chudy <chudyja1@fit.cvut.cz>
2023-04-19 02:27:39 +0200

    Using launch_ros.substitutions instead of 'os' library

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -1,24 +1,23 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-
-from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 RVIZ_PKG = "rr1_rviz"
 RVIZ_CONFIG_FILE = "urdf_viz_topic.rviz"
 
 def generate_launch_description():
-    rviz_pkg = get_package_share_directory(RVIZ_PKG)
-
     use_sim_time = LaunchConfiguration('use_sim_time')
     declare_use_sim_time = DeclareLaunchArgument(
         'use_sim_time',
         default_value='false',
         description='Use simulation (Gazebo) clock if true')
 
-    rviz_config = os.path.join(rviz_pkg, "rviz", RVIZ_CONFIG_FILE)
+    rviz_config = PathJoinSubstitution([
+        FindPackageShare(RVIZ_PKG), "rviz", RVIZ_CONFIG_FILE
+        ])
+    
     rviz = Node(
         package="rviz2",
         executable="rviz2",
